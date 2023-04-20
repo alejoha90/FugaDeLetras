635ef6c683724dcb09e77790317489a4f49d93e6
alejoha90 <alejandro.herran@correounivalle.edu.co>
2023-04-19 21:35:30 -0500

    inicio del proyecto

---
+++ src/fuga/de/letras/FugaDeLetras.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package fuga.de.letras;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class FugaDeLetras {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Ventana v1 = new Ventana();
+    v1.setSize(500,500);
+    v1.setLocationRelativeTo(null);
+    v1.setVisible(true);
+    }
+    
+}
